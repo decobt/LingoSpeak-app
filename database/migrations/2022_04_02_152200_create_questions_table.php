b9073b2a1f62b67cf2b21d64321e8b1fa5d84dfe
Trayche <roskoskitrajce@gmail.com>
2022-04-02 18:46:36 +0200

    created questions model and seeder

---
+++ database/migrations/2022_04_02_152200_create_questions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->text('question');
+            $table->text('answer');
+            $table->json('choices')->nullable();
+            $table->bigInteger('topic_id')->unsigned()->nullable();
+            $table->timestamps();
+
+            $table->foreign('topic_id')->references('id')->on('topics')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+};
