b1235d9eb692f46930a3b492219441f0c8ddce11
Trayche <roskoskitrajce@gmail.com>
2022-04-15 20:48:29 +0200

    added question seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(TopicsSeeder::class);
+        $this->call(QuestionSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
--- database/seeders/QuestionSeeder.php
+++ database/seeders/QuestionSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
 class QuestionSeeder extends Seeder
@@ -14,6 +14,25 @@ class QuestionSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('questions')->insert([
+            'question' => 'What is the name for bread?',
+            'choices' => json_encode([ 'a' => 'djathë', 'b'=> 'bukë', 'c'=> 'sheqer', 'd'=> 'vezë' ]),
+            'answer' => 'b',
+            'topic_id' => 23
+        ]);
+
+        DB::table('questions')->insert([
+            'question' => 'What is the name for breakfast?',
+            'choices' => json_encode([ 'a' => 'drekë', 'b'=> 'ushqim', 'c'=> 'mëngjes', 'd'=> 'darkë' ]),
+            'answer' => 'c',
+            'topic_id' => 23
+        ]);
+
+        DB::table('questions')->insert([
+            'question' => 'What is the name for dinner?',
+            'choices' => json_encode([ 'a' => 'mëngjes', 'b'=> 'erëza', 'c'=> 'pjatë', 'd'=> 'darkë' ]),
+            'answer' => 'd',
+            'topic_id' => 23
+        ]);
     }
 }
